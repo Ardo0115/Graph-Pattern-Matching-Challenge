9d3b3b3d13ade1004af31282c8b8e20776522e88
Ardo0115 <ardo0115@gmail.com>
2021-06-05 17:42:38 +0900

    Fix main, compile option

--- main/main.cc
+++ main/main.cc
@@ -11,21 +11,16 @@
 #include <fstream>
 
 int main(int argc, char* argv[]) {
- if (argc < 4) {
-   std::cerr << "Usage: ./program <data graph file> <query graph file> "
-                "<candidate set file>\n";
-   return EXIT_FAILURE;
- }
+  if (argc < 4) {
+    std::cerr << "Usage: ./program <data graph file> <query graph file> "
+                  "<candidate set file>\n";
+    return EXIT_FAILURE;
+  }
+  
+  std::string data_file_name = argv[1];
+  std::string query_file_name = argv[2];
+  std::string candidate_set_file_name = argv[3];
  
- std::string data_file_name = argv[1];
- std::string query_file_name = argv[2];
- std::string candidate_set_file_name = argv[3];
-// lcc_human, lcc_hprd, lcc_yeast
-// test_data.igraph, test_query.igraph, test_cs.cs
-    // std::string data_file_name = "C:\\Users\\jkj72\\Desktop\\graphChallenge\\Graph-Pattern-Matching-Challenge\\data\\lcc_yeast.igraph";
-    // std::string query_file_name = "C:\\Users\\jkj72\\Desktop\\graphChallenge\\Graph-Pattern-Matching-Challenge\\query\\lcc_yeast_s3.igraph";
-    // std::string candidate_set_file_name = "C:\\Users\\jkj72\\Desktop\\graphChallenge\\Graph-Pattern-Matching-Challenge\\candidate_set\\lcc_yeast_s3.cs";
-
   Graph data(data_file_name);
   Graph query(query_file_name, true);
   CandidateSet candidate_set(candidate_set_file_name);
