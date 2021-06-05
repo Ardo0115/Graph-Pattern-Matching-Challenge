c5370272227d58a89aa76c39adbfa720c314229c
Ardo0115 <ardo0115@gmail.com>
2021-06-05 21:52:49 +0900

    Fix testhprdn1

--- test_hprd_n1.sh
+++ test_hprd_n1.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-#i=1
-#
-#while [ $i -le 100 ]
-#do
-#    ./build/main/program data/lcc_hprd.igraph query/lcc_hprd_n1.igraph candidate_set/lcc_hprd_n1.cs ../jkj_decision2.txt
-#    ((i++))
-#done
+i=1
 
-# hprd n
-./build/main/program data/lcc_hprd.igraph query/lcc_hprd_n1.igraph candidate_set/lcc_hprd_n1.cs ../tmp_test.txt
-sleep 5
-ps -ef | grep program | grep -v grep | awk '{print $2}' | xargs kill
-./build/main/program data/lcc_hprd.igraph query/lcc_hprd_n3.igraph candidate_set/lcc_hprd_n3.cs ../tmp_test.txt
-sleep 5
-ps -ef | grep program | grep -v grep | awk '{print $2}' | xargs kill
-./build/main/program data/lcc_hprd.igraph query/lcc_hprd_n5.igraph candidate_set/lcc_hprd_n5.cs ../tmp_test.txt
-sleep 5
-ps -ef | grep program | grep -v grep | awk '{print $2}' | xargs kill
-./build/main/program data/lcc_hprd.igraph query/lcc_hprd_n8.igraph candidate_set/lcc_hprd_n8.cs ../tmp_test.txt
-sleep 5
-ps -ef | grep program | grep -v grep | awk '{print $2}' | xargs kill
+while [ $i -le 10 ]
+do
+    ./build/main/program data/lcc_hprd.igraph query/lcc_hprd_n1.igraph candidate_set/lcc_hprd_n1.cs ../final_wo_weight.txt
+    ((i++))
+done
